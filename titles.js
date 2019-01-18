f0466ba90f9a2c97c02a2dfb7a0e08060603d4f2
Zachary Yaro <zmyaro@zmyaro.com>
2019-01-17 22:59:38 -0500

    Initial commit

---
+++ titles.js
@@ -0,0 +1,33 @@
+var TITLES = [
+	{
+		creator: 'Howl\'s',
+		title: 'Moving Castle'
+	}, {
+		creator: 'James Cameron\'s',
+		title: 'Avatar'
+	}, {
+		creator: 'Lemony Snicket\'s',
+		title: 'A Series Of Unfortunate Events'
+	}, {
+		creator: 'Mordenkainen\'s',
+		title: 'Tome Of Foes'
+	}, {
+		creator: 'Muppets',
+		title: 'Most Wanted'
+	}, {
+		creator: 'Ripley\'s',
+		title: 'Believe It Or Not'
+	}, {
+		creator: 'Sid Meier\'s',
+		title: 'Civilization'
+	}, {
+		creator: 'Strong Bad\'s',
+		title: 'Cool Game For Attractive People'
+	}, {
+		creator: 'Tom Clancy\'s',
+		title: 'Ghost Recon'
+	}, {
+		creator: 'Tom Clancy\'s',
+		title: 'Rainbow Six'
+	}
+];
